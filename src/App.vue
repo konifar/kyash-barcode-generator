a1109c4ada58075be820d5a1de62fb3336596c03
konifar <yahpeycoy0403@gmail.com>
2020-11-29 02:17:04 +0900

    Introduce vue-qrcode-reader

--- src/App.vue
+++ src/App.vue
@@ -34,24 +34,89 @@
     </v-app-bar>
 
     <v-main>
-      <HelloWorld />
+      <p class="decode-result">Last result: <b>{{ result }}</b></p>
+
+      <p v-if="error !== null" class="drop-error">
+        {{ error }}
+      </p>
+
+      <QrcodeDropZone @detect="onDetect" @dragover="onDragOver" @init="logErrors">
+        <div class="drop-area" :class="{ 'dragover': dragover }">
+          DROP SOME IMAGES HERE
+        </div>
+      </QrcodeDropZone>
+      <QrcodeCapture @decode="onDecode" />
     </v-main>
   </v-app>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
+import { QrcodeDropZone, QrcodeCapture } from "vue-qrcode-reader";
 import HelloWorld from "./components/HelloWorld.vue";
 
 export default Vue.extend({
   name: "App",
 
   components: {
-    HelloWorld
+    QrcodeDropZone,
+    QrcodeCapture
   },
 
   data: () => ({
-    //
-  })
+    result: "",
+    error: "",
+    dragover: false
+  }),
+  
+  methods: {
+    onDecode (decodedString: string) {
+      this.result = decodedString
+    },
+    async onDetect (promise: Promise<string>) {
+      try {
+        const { content } = await promise
+        this.result = content
+        this.error = ""
+      } catch (error) {
+        if (error.name === 'DropImageFetchError') {
+          this.error = 'Sorry, you can\'t load cross-origin images :/'
+        } else if (error.name === 'DropImageDecodeError') {
+          this.error = 'Ok, that\'s not an image. That can\'t be decoded.'
+        } else {
+          this.error = 'Ups, what kind of error is this?! ' + error.message
+        }
+      }
+    },
+
+    logErrors (promise: Promise<string>) {
+      promise.catch(console.error)
+    },
+
+    onDragOver (isDraggingOver: boolean) {
+      this.dragover = isDraggingOver
+    }
+  }
 });
 </script>
+
+<style>
+.drop-area {
+  height: 300px;
+  color: #fff;
+  text-align: center;
+  font-weight: bold;
+  padding: 10px;
+
+  background-color: rgba(0,0,0,.5);
+}
+
+.dragover {
+  background-color: rgba(0,0,0,.8);
+}
+
+.drop-error {
+  color: red;
+  font-weight: bold;
+}
+</style>
